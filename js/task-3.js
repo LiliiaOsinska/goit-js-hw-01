01226927194a030b28a62647f7f5426547dc0305
LiliiaOsinska <lo.lihcka@gmail.com>
2024-09-22 18:59:10 +0200

    add file js

---
+++ js/task-3.js
@@ -0,0 +1,9 @@
+function getElementWidth(content, padding, border) {
+    const boxSizing = Number.parseFloat(content) +
+    (Number.parseFloat(padding) +
+    Number.parseFloat(border)) * 2;
+    return boxSizing;
+}
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
